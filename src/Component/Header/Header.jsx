b3b72aef4ebb9de9905e5ed6b7602b6b521bf319
AmdadulHQ <rimonamdadul301@gmail.com>
2024-04-26 09:05:59 +0600

    adding login and log out in navbar

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,6 +1,29 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../Context/ContextComponent";
+import Swal from "sweetalert2";
 
 const Header = () => {
+    const { user , logOut } = useContext(AuthContext)
+
+    const handleLogOut = () => {
+        logOut()
+        .then(()=>{
+            Swal.fire({
+                icon: "success",
+                title: "Good Job!",
+                text: "Log Out Successfully",
+            });
+        })
+        .catch(error => {
+            console.log(error.message);
+            return Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: `${error.message.split('/')[1].split(')')[0].replace(/-/g,' ')}`,
+              });
+        })
+    }
     return (
         <header className="max-w-[1440px] mx-auto">
             <nav className="font-Montserrat flex justify-between py-5 items-center">
@@ -13,7 +36,9 @@ const Header = () => {
                 </ul>
                 <div className="text-base font-medium space-x-6">
                     <NavLink to='/register'>Register</NavLink>
-                    <NavLink to='/login'>LogIn</NavLink>
+                    {
+                        user ? <NavLink onClick={handleLogOut}>Log Out</NavLink> : <NavLink to='/login'>Log in</NavLink>
+                    }
                 </div>
             </nav>   
         </header>
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,11 +1,35 @@
 
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../Context/ContextComponent';
+import Swal from 'sweetalert2';
+import { IoEye , IoEyeOff  } from "react-icons/io5";
 
 const Register = () => {
 
+    const [showPassword,setShowPassword] = useState(false);
+    const [showConfirmPassword,setShowConfirmPassword] = useState(false)
+
+    const pattern = /(?=.*[A-Z])(?=.*[a-z]).{6,}$/
+
     const { createUser , logOut , updateUserProfile } = useContext(AuthContext)
+
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const history = location.state?.from || '/login';
+
+    const navigateNow = () => {
+        setTimeout(() => { navigate(history, { replace: true }) }, 1)
+    }
+
+
+    const handleShowPassword = () => {
+        setShowPassword(!showPassword)
+    }
+    const handleConfirmShowPassword =() => {
+        setShowConfirmPassword(!showConfirmPassword)
+    }
 
     const handleRegister = e => {
         e.preventDefault()
@@ -15,14 +39,56 @@ const Register = () => {
         const lastName = form.lastName.value;
         const fullName = firstName+' '+lastName;
         const email = form.email.value;
-        const firstPassword = form.password.value;
+        const password = form.password.value;
         const repeat_password = form.repeat_password.value;
         const photo = form.photo.value;
-        if(firstPassword !== repeat_password){
-            return 
+        if(password !== repeat_password){
+            return Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Password Does't Match",
+              });
+        }
+        if(!pattern.test(repeat_password)){
+            return Swal.fire({
+                icon:"error",
+                title:"Oops",
+                text:"Password Should Have an Uppercase & Lowercase letter, It length should be more then 6 character"
+            })
         }
 
         createUser(email,password)
+        .then(result => {
+            updateUserProfile(fullName,photo)
+            .then(()=>{
+                Swal.fire({
+                    icon: "success",
+                    title: "Good Job!",
+                    text: "Account Created Successfully",
+                });
+                logOut()
+                .then(()=>{
+
+                })
+                navigateNow()
+            })
+            .catch(error => {
+                console.log(error.message);
+                return Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: `${error.message.split('/')[1].split(')')[0].replace(/-/g,' ')}`,
+                  });
+            })
+        })
+        .catch(error => {
+            console.log(error.message);
+            return Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: `${error.message.split('/')[1].split(')')[0].replace(/-/g,' ')}`,
+              });
+        })
 
     }
 
@@ -50,19 +116,35 @@ const Register = () => {
                     <label htmlFor="email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8">Email address</label>
                 </div>
                 <div className="relative z-0 w-full mb-5 group">
-                    <input type="password" name="password" id="password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <input type={showPassword ? "text" : "password"} name="password" id="password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label htmlFor="password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8">Password</label>
+                    <div onClick={handleShowPassword} className='absolute right-0 top-[30%] text-xl'>
+                        {
+                            showPassword ? <IoEyeOff/> : <IoEye/>
+                        }
+                    </div>
                 </div>
                 <div className="relative z-0 w-full mb-5 group">
-                    <input type="password" name="repeat_password" id="repeat_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <input type={showConfirmPassword ? "text" : "password"} name="repeat_password" id="repeat_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label htmlFor="repeat_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8">Confirm password</label>
+                    <div onClick={handleConfirmShowPassword} className='absolute right-0 top-[30%] text-xl'>
+                        {
+                            showConfirmPassword ? <IoEyeOff/> : <IoEye/>
+                        }
+                    </div>
                 </div>
                 <div className="flex items-center h-5">
                     <input id="terms" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
                     <label htmlFor="terms" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a></label>
                 </div>
-                <div className='flex justify-center'>
-                <button type="submit" className="text-white my-6 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign Up</button>
+                <div className='flex justify-center my-5'>
+                    <button onSubmit={handleRegister} type="submit" className="relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-black border-2 border-black rounded-full hover:text-white group hover:bg-gray-50">
+                        <span className="absolute left-0 block w-full h-0 transition-all bg-black opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+                        <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
+                        {/* <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg> */}
+                        </span>
+                        <span className="relative">Sign Up</span>
+                    </button>
                 </div>
             </form>
             <div>
