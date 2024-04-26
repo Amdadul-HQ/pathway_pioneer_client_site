7d381df9d1b86c113721ac6ad121c17d861a87c5
AmdadulHQ <rimonamdadul301@gmail.com>
2024-04-27 00:41:14 +0600

    adding view Details sectionn

--- src/Pages/MyaddedList/MyAddedList.jsx
+++ src/Pages/MyaddedList/MyAddedList.jsx
@@ -2,28 +2,86 @@ import React from 'react';
 import { IoEyeSharp } from 'react-icons/io5';
 import { MdDelete, MdEdit } from 'react-icons/md';
 import { Link, useLoaderData } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const MyAddedList = () => {
     const list = useLoaderData()
+
+    
+
+    const handleDeletePlace = () =>{
+        const handleDeleteCoffee = id => {
+            const swalWithBootstrapButtons = Swal.mixin({
+                customClass: {
+                  confirmButton: "btn btn-success",
+                  cancelButton: "btn btn-danger",
+                  
+                  popup: 'custom-font',
+                  title: 'custom-font',
+                  content: 'custom-font',
+                  confirm: 'custom-font',
+                  cancel: 'custom-font'
+                },
+                buttonsStyling: false
+              });
+              swalWithBootstrapButtons.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonText: "Yes, delete it!",
+                cancelButtonText: "No, cancel!",
+                reverseButtons: true
+              }).then((result) => {
+                if (result.isConfirmed) {
+                    fetch(`https://coffee-store-server-site-five.vercel.app/coffee/${id}`,{
+                        method:'DELETE'
+                    })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if(data.deletedCount>0){
+                               swalWithBootstrapButtons.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        const newCoffeeStore = coffeeStore.filter(coffee => coffee._id !== id );
+                        setCoffeeStore(newCoffeeStore)
+                        }
+                    })
+                    
+                } else if (
+                  /* Read more about handling dismissals below */
+                  result.dismiss === Swal.DismissReason.cancel
+                ) {
+                  swalWithBootstrapButtons.fire({
+                    title: "Cancelled",
+                    text: "Your imaginary file is safe :)",
+                    icon: "error"
+                  });
+                }
+              });
+    }}
     // country,tourists_spot_name,spot_location,short_description,bordered_radio,totalVisitorsPerYear,photourl,travel_time,average_cost,email,userName
     return (
         <section className='max-w-[1440px] mx-auto'>
-            <h1>My Added Place</h1>
-            <div>
+            <h1 className='text-center my-10 font-Bebas text-5xl '>My Added Place</h1>
+            <div className='grid grid-cols-3 gap-6'>
                 {
-                    list && list.map( item => <div className="flex items-center justify-between bg-[#F5F4F1] rounded-xl p-5" key={item._id}>    
+                    list && list.map( item => <div className="items-center justify-between bg-[#F5F4F1] rounded-xl p-5" key={item._id}>    
                     <div>
-                        <img className="w-[185px] h-[240px]" src={item.photourl} alt="" />
+                        <img className="w-full h-[300px] rounded-md" src={item.photourl} alt="" />
                     </div>
-                    <div className="font-Raleway">
-                        <p className="text-[#5C5B5B] text-xl font-normal"><span className="text-[#1B1A1A] text-xl font-semibold">Name:</span> {item.itemName}</p>
-                        <p className="text-[#5C5B5B] text-xl font-normal"><span className="text-[#1B1A1A] text-xl font-semibold">Chef:</span> {item.chefName}</p>
-                        <p className="text-[#5C5B5B] text-xl font-normal"><span className="text-[#1B1A1A] text-xl font-semibold">Price:</span> {item.price} Taka</p>
+                    <div className="font-Montserrat mt-2">
+                        <p className="text-[#5C5B5B] text-lg font-normal font-Montserrat uppercase"><span className="text-[#1B1A1A] text-base font-semibold">Country :</span> {item.country}</p>
+                        <p className="text-[#5C5B5B] text-lg font-normal font-Montserrat uppercase"><span className="text-[#1B1A1A] text-base font-semibold">Tourist Spot Name: </span> {item.tourists_spot_name}</p>
+                        <p className="text-[#5C5B5B] text-lg font-normal font-Montserrat uppercase"><span className="text-[#1B1A1A] text-base font-semibold">Total Visitors Per year: </span> {item.totalVisitorsPerYear}</p>
                     </div>
-                    <div className="flex flex-col justify-around gap-6 font-Raleway">
-                        <Link to={`/item/${item._id}`} className="bg-[#D2B48C] p-3 text-white text-2xl rounded-xl"><IoEyeSharp/></Link>
-                        <Link to={`/updateitem/${item._id}`} className="bg-[#3C393B] p-3 text-white text-2xl rounded-xl"><MdEdit/></Link>
-                        <Link onClick={()=> handleDeleteitem(item._id)} className="bg-[#EA4744] font-Raleway p-3 text-white text-2xl rounded-xl"><MdDelete/></Link>
+                    <div className="flex justify-between gap-6 font-Raleway mt-3 font-Montserrat">
+                        <Link to={`/item/${item._id}`} className="bg-[#D2B48C] p-3 w-full text-white text-2xl rounded-xl text-center justify-center flex"><IoEyeSharp className='text-center'/></Link>
+                        <Link to={`/updateitem/${item._id}`} className="bg-[#3C393B] p-3 w-full text-white text-2xl rounded-xl text-center justify-center flex"><MdEdit className='text-center'/></Link>
+                        <Link onClick={()=> handleDeletePlace(item._id)} className="bg-[#EA4744] w-full font-Raleway p-3 text-white text-2xl rounded-xl text-center justify-center flex"><MdDelete className='text-center'/></Link>
                     </div>
                 </div> )
                 }
--- src/Pages/ViewTouristsSpot/ViewTouristsSpot.jsx
+++ src/Pages/ViewTouristsSpot/ViewTouristsSpot.jsx
@@ -2,11 +2,35 @@ import { useLoaderData } from "react-router-dom";
 
 const ViewTouristsSpot = () => {
     const spot = useLoaderData()
-    console.log(spot);
+    // console.log(spot);
+    // country,tourists_spot_name,spot_location,short_description,bordered_radio,totalVisitorsPerYear,photourl,travel_time,average_cost,email,userName
     return (
-        <div>
-            
-        </div>
+        <section className="max-w-[1440] mx-auto my-20 font-Montserrat">
+               <div className="grid grid-cols-4 justify-center items-center">
+                    <div className="flex justify-center items-center">
+                        <div>
+                            <h1 className="uppercase">{spot.country}</h1>
+                            <p>Tourist Spot Name : {spot.tourists_spot_name}</p>
+                            <p>Location:{spot.spot_location}</p>
+                            <p>Description: {spot.short_description}</p>
+                            <p></p>
+                        </div>
+                    </div>
+                    <div className="col-span-2">
+                        <img className="w-full mx-auto rounded-md" src={spot.photourl} alt="" />
+                    </div>
+                    <div className="flex justify-center items-center">
+                        <div>
+                            <p>Seasonality : {spot.border_radio}</p>
+                            <p>Discription : {spot.short_description}</p>
+                            <p>Travel Time : {spot.travel_time}</p>
+                            <p>Total Visitor Per Year : {spot.totalVisitorsPeryear}</p>
+                            <p>Average Cost : {spot.average_cost}</p>
+                        </div>
+                    </div>
+
+                </div>
+        </section>
     );
 };
 
